c404e20a6a2d347e34f982201764d5bb29346deb
Samantha Perry <samnperry@arizona.edu>
2024-06-04 13:57:28 +0300

    Update new script

--- rad_analysis_p2.sh
+++ rad_analysis_p2.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 PATH_HERE=C:/Users/SamP9/OneDrive/Documents/GitHub/train-ticket
-OUTPUT_FILE="output_all_commits.json"
 
-cd "$PATH_HERE"
+BASE_DIR=$(dirname "$0")
+OUTPUT_FOLDER="$BASE_DIR/sample_output"
+OUTPUT_FILE="output_commit_"
+
+cd "$PATH_HERE" || exit
+
+# Create the output folder if it doesn't exist
+mkdir -p "$OUTPUT_FOLDER"
 
 git fetch origin
 
 DEFAULT_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
 
-COMMIT_HASHES=$(git rev-list "origin/$DEFAULT_BRANCH")
-
-> "$OUTPUT_FILE"
+COMMIT_HASHES=$(git rev-list --reverse "origin/$DEFAULT_BRANCH")
 
 for COMMIT_HASH in $COMMIT_HASHES; do
     git checkout $COMMIT_HASH
@@ -19,8 +23,7 @@ for COMMIT_HASH in $COMMIT_HASHES; do
         --url http://localhost:8080/ \
         --header 'content-type: application/json' \
         --data "{
-        \"commitHash\": \"$COMMIT_HASH\",
         \"pathToMsRoots\": [\"${PATH_HERE}\"]
-    }" >> "$OUTPUT_FILE"
+    }" > "${OUTPUT_FOLDER}/${OUTPUT_FILE}${COMMIT_HASH}"
 
 done
